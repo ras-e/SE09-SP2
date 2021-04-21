185488e01a0256133c437f915b210617d315daf6
Christoffer <chrisprosniper@gmail.com>
2021-04-21 16:27:05 +0200

    constructer update

--- src/main/java/domain/user/Account.java
+++ src/main/java/domain/user/Account.java
@@ -1,9 +1,16 @@
 package domain.user;
 
 public abstract class Account {
+    public String email;
+    public String firstName;
+    public String lastName;
+    public int id;
 
-    public Account (String email, String name, int id) {
-
+    public Account (String email, String firstName, String lastName, int id) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
     }
     
 }
