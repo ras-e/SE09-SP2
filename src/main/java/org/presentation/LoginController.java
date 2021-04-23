9074148c61c7aa85916d83cd16e3d8a03c199ca4
Rasse <68448027+r-jac@users.noreply.github.com>
2021-04-23 23:18:38 +0200

    Login UI done

--- src/main/java/org/presentation/LoginController.java
+++ src/main/java/org/presentation/LoginController.java
@@ -1,7 +1,54 @@
 package org.presentation;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class LoginController {
+
+    @FXML
+    public AnchorPane layersignup;
+    @FXML
+    public TextField tfEmail;
+    @FXML
+    public TextField tfPass;
+    @FXML
+    public Button btnSignIn;
+    @FXML
+    public Button btnSignUp;
+    @FXML
+    public Pane pgSignUp;
+    @FXML
+    public Pane pgSignIn;
+    @FXML
+    public Button btnBack;
+    @FXML
+    public RadioButton bntExit;
+
+    @FXML
+    private void handleButtonAction (ActionEvent event) {
+        if (event.getSource().equals(btnSignUp)) {
+            pgSignUp.toFront();
+        }
+    }
+
+    @FXML
+    private void handleMouseEvent (MouseEvent event) {
+        if (event.getSource() == bntExit) {
+            System.exit(0); }
+        if (event.getSource().equals(btnBack))
+        { pgSignIn.toFront();
+        }
+    }
 }
+
+
