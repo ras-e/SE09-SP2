7cbd5054461edafd50568b36f894b344444f8650
Rasse <68448027+r-jac@users.noreply.github.com>
2021-04-24 22:18:06 +0200

    fix return

--- src/main/java/Facade/ioFacade.java
+++ src/main/java/Facade/ioFacade.java
@@ -29,7 +29,7 @@ public class ioFacade implements Serializable {
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(ioFacade.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
+        return l;
     }
 
     public void writeobjecttofile(Credits credits, String filename) {
