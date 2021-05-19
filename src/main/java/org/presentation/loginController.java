a4822e3e2cfb31d003b8447ae7b9766288e71aa1
Rasse <68448027+r-jac@users.noreply.github.com>
2021-05-19 15:40:14 +0200

    login fix

--- src/main/java/org/presentation/loginController.java
+++ src/main/java/org/presentation/loginController.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
