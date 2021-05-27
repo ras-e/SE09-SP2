a8355a06341bfee0a0b3ce999f2540a9046f33fc
andenbat <andersbat@live.dk>
2021-05-27 14:26:46 +0200

    ikke færdigt arbejde med gdpr knap til rasmus

--- src/main/java/org/presentation/App.java
+++ src/main/java/org/presentation/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("adminUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
         //primaryStage.setTitle("Hello World");
         Scene scene = new Scene(root);
